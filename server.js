9fd683c33fdfc48559a2b14dc0974acc5468c35c
ICB CINTA TEKNIKA <kangkomaganteng@gmail.com>
2024-02-10 14:41:30 +0700

    setting router di server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// memanggil file routers
+var routes = require('./routers');
+    routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
